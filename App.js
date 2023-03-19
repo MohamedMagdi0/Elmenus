0ba442d51f7995958df67586d7a5499a1767dc64
MohamedMagdi0 <MohamedMagdi0>
2023-03-19 02:45:35 +0200

    refactor: clean up

--- App.js
+++ App.js
@@ -1,70 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
-import React from 'react';
-// import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {HomeScreen} from './src/pages';
+import {Colors} from './src/constants';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import Home from './src/pages/Home/Home';
-import HomeScreen from './src/pages/Home/HomeScreen';
-import COLORS from './src/constants/Colors';
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <>
-      <StatusBar
-        style="auto"
-        translucent={false}
-        backgroundColor={COLORS.GRAY}
-      />
-      {/* <SafeAreaView style={backgroundStyle}> */}
-
-      <HomeScreen />
+      <StatusBar backgroundColor={Colors.BASIC_BACKGROUND} />
+      <NavigationContainer>
+        <HomeScreen />
+      </NavigationContainer>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
